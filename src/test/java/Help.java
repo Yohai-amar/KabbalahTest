58bcf657a6dc80db36f9b267037227b182fcbb00
yohai <yohayi.amar@gmail.com>
2022-03-31 18:25:39 +0300

    for the friends

---
+++ src/test/java/Help.java
@@ -0,0 +1,6 @@
+public class Help {
+
+    static final String KABBALAHMEDIASITE = "https://kabbalahmedia.info/he/";
+    static final String DAILYLESSONCSS = "#root > div > div.layout__main > div.layout__content > div > div.homepage__featured.homepage__section > div > div > div > div:nth-child(1) > div > a";
+    static final String LATESTLESSONPAGE = "https://kabbalahmedia.info/he/lessons/daily/latest";
+}
---
+++ src/test/java/KabbalahTest.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class KabbalahTest {
+
+
+    @BeforeClass
+    public static void setupFirst(){
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.safaridriver().setup();
+    }
+
+    @Test
+    public void dailyLesson(){
+        WebDriver driver = new ChromeDriver();
+        driver.get(Help.KABBALAHMEDIASITE);
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement dailyLessonBtn = wait.
+                until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.DAILYLESSONCSS)));
+        dailyLessonBtn.click();
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertEquals(newUrl,Help.LATESTLESSONPAGE);
+        driver.quit();
+    }
+}
