b2f5181b9b0bb2126bfa96d2e1c502c76ee20105
yohai <yohayi.amar@gmail.com>
2022-04-01 09:16:32 +0300

    now

--- src/test/java/Help.java
+++ src/test/java/Help.java
@@ -1,8 +1,24 @@
 public class Help {
-
+//                                                      קבלה מדיה אתר ואלמנטים
     static final String KABBALAHMEDIASITE = "https://kabbalahmedia.info/he/";
     static final String DAILYLESSONCSS = "#root > div > div.layout__main > div.layout__content > div > div.homepage__featured.homepage__section > div > div > div > div:nth-child(1) > div > a";
     static final String LATESTLESSONPAGE = "https://kabbalahmedia.info/he/lessons/daily/latest";
     static final String OPENINGWISDOMPICTXTLINK = "פתיחה לחכמת הקבלה";
     static final String OPENINGWISDOMPAGE = "https://kabbalahmedia.info/he/lessons/series/c/ReQUUOtN";
+
+
+
+    //                                                  הלב רואה אתר ואלמנטים
+    static final String HALEVROEESITE = "https://www.kab1.com/events/the-heart-sees/he";
+    static final String MASAINISRAELWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(1) > a > div.thumb-image-wrapper > img";
+    static final String HASADNAWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(2) > a > div.thumb-image-wrapper > img";
+    static final String MASHUKAZEWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(3) > a > div.thumb-image-wrapper > img";
+    static final String INTEGRALEDUWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(4) > a > div.thumb-image-wrapper > img";
+    static final String MOVIETRAILERCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(5) > a > div.thumb-image-wrapper > img";
+    static final String MADEINISRAELWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(6) > a > div.thumb-image-wrapper > img";
+    static final String STUDYKABWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(7) > a > div.thumb-image-wrapper > img";
+    static final String ONLINEVIDSWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(8) > a > div.thumb-image-wrapper > img";
+    static final String INNERVIEWWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(9) > a > div.thumb-image-wrapper > img";
+    static final String MUSICWINDOWCSS = "#w-node-_11690ad2-6aee-7f6f-c4c9-6f44e1ca7bb7-77834ae7 > div > div:nth-child(10) > a > div.thumb-image-wrapper > img";
+
 }
--- src/test/java/KabbalahTest.java
+++ src/test/java/KabbalahTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -19,7 +21,7 @@ public class KabbalahTest {
         WebDriverManager.chromedriver().setup();
         WebDriverManager.safaridriver().setup();
     }
-
+//                      טסט שפותח את דף השיעור היומי
     @Test
     public void dailyLesson(){
         WebDriver driver = new ChromeDriver();
@@ -33,7 +35,7 @@ public class KabbalahTest {
         Assert.assertEquals(newUrl,Help.LATESTLESSONPAGE);
         driver.quit();
     }
-
+//                             פתיחה למבוא לחכמת הקבלה דדרך הטאב
     @Test
     public void openingWisdomTab(){
         WebDriver driver = new ChromeDriver();
@@ -47,4 +49,53 @@ public class KabbalahTest {
         Assert.assertEquals(newUrl,Help.OPENINGWISDOMPAGE);
         driver.quit();
     }
+
+    //                     טסט לבדיקת העכבר כשהוא מרחף מעל הטאב והאם הוא מתחלף לטקסט
+
+    @Test
+    public void hoverMouse() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get(Help.HALEVROEESITE);
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        Actions action = new Actions(driver);
+        WebElement masaBeIsrael = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.MASAINISRAELWINDOWCSS)));
+        WebElement hasadna = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.HASADNAWINDOWCSS)));
+        WebElement mashuKaze = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.MASHUKAZEWINDOWCSS)));
+        WebElement integralEdu = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.HASADNAWINDOWCSS)));
+        WebElement movieTrailer = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.MOVIETRAILERCSS)));
+        WebElement madeInIsrael = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.MADEINISRAELWINDOWCSS)));
+        WebElement studyKab = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.STUDYKABWINDOWCSS)));
+        WebElement onlineVids = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.ONLINEVIDSWINDOWCSS)));
+        WebElement innerView = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.INNERVIEWWINDOWCSS)));
+        WebElement music = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Help.MUSICWINDOWCSS)));
+        Thread.sleep(3000);
+        action.moveToElement(masaBeIsrael).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(hasadna).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(mashuKaze).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(integralEdu).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(movieTrailer).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(madeInIsrael).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(studyKab).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(onlineVids).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(innerView).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        action.moveToElement(music).clickAndHold().build().perform();
+        Thread.sleep(3000);
+        driver.quit();
+
+
+
+    }
+
+
+
 }
